a912e55217dcf29075b320fac75bc494bf188e6f
Zaid Sohel Ajani <zaidajani2007@gmail.com>
2022-07-08 22:37:15 +0530

    making login screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { LogoutView } from './screens/loggedOut.screen';
+import { Login } from './screens/login.screen';
 import { StartupDetail } from './screens/startupDetail.screen';
 
 function App() {
   return (
     <>
-      <LogoutView />
+      <Login />
     </>
   );
 }
---
+++ src/screens/login.screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./login.style.css";
+
+export const Login = () => {
+  return (
+    <>
+      <div className="loginContainer">
+        <div className="loginCard"></div>;
+      </div>
+    </>
+  );
+};
